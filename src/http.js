7aa0975939d8e68183cef8b3d47591560a4a3d8a
hbrls <shuaizhexu@gmail.com>
2019-07-12 11:50:55 +0800

    A: webpayload

--- src/http.js
+++ src/http.js
@@ -22,5 +22,37 @@ $http.delete = function (endpoint, query = {}) {
   return $http._delete(endpoint, { params: query });
 };
 
+// more sugar
+function once($, options) {
+  return {
+    post: (endpoint, command) => $.post(endpoint, command, options),
+  };
+}
+
+$http.xpl = function () {
+  const headers = {};
+
+  let payload = null;
+  if (document && document.querySelector) {
+    const input = document.querySelector('#_webpayload');
+    if (input) {
+      const value = input.value;
+      if (value) {
+        payload = value;
+      }
+    }
+  } else if (localStorage && localStorage.getItem && localStorage.setItem) {
+    const item = localStorage.getItem('#_webpayload');
+    if (item) {
+      payload = item;
+    }
+  }
+
+  if (payload) {
+    headers['X-Pl'] = payload;
+  }
+
+  return once($http, { headers });
+};
 
 export default $http;
