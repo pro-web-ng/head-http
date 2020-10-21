ff4ae05a6aa5c20df929f657a9e3b516f6a7c30a
理斯特 <shuaizhexu@gmail.com>
2020-10-21 17:05:23 +0800

    A: $http query data

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 @head/http
 ==
+
+```javascript
+$http.get(endpoint);
+$http.get(endpoint, query);
+$http.get(endpoint, query, options);
+$http.get(endpoint, null, options);
+
+$http.post(endpoint);
+$http.post(endpoint, data);
+$http.post(endpoint, query, data);
+$http.post(endpoint, query, data, options);
+$http.post(endpoint, null, data, options);
+```
+
+CHANGELOG
+==
+
+0.4.0
+--
+
+1. 显式支持了 `query` 和 `data` 的语法糖
--- src/http.js
+++ src/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { merge } from './axios/lib/utils';
 import token from './token';
 import { onSuccess, onError } from './response';
 
@@ -23,14 +24,42 @@ $http.interceptors.response.use(onSuccess, onError);
 // sugar
 $http._get = $http.get;
 
-$http.get = function (endpoint, query = {}) {
-  return $http._get(endpoint, { params: query });
+$http.get = function (endpoint, query = {}, options = {}) {
+  return $http._get(endpoint, merge(options, { params: query }));
+};
+
+$http._post = $http.post;
+
+$http.post = function (...args) {
+  const [endpoint, , , options] = args;
+  let [, query, data] = args;
+  if (!options) {
+    if (!data) {
+      data = query;
+      query = {};
+    }
+  }
+  return $http._post(endpoint, data, merge(options, { params: query }));
+};
+
+$http._put = $http.put;
+
+$http.put = function (...args) {
+  const [endpoint, , , options] = args;
+  let [, query, data] = args;
+  if (!options) {
+    if (!data) {
+      data = query;
+      query = {};
+    }
+  }
+  return $http._put(endpoint, data, merge(options, { params: query }));
 };
 
 $http._delete = $http.delete;
 
-$http.delete = function (endpoint, query = {}) {
-  return $http._delete(endpoint, { params: query });
+$http.delete = function (endpoint, query = {}, options = {}) {
+  return $http._delete(endpoint, merge(options, { params: query }));
 };
 
 export default $http;
